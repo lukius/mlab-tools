1db2ed9c6814b96ef40021dccc776f7f9e709af1
Lucio Santi <lukius@gmail.com>
2017-07-07 17:18:57 -0300

    Automatic video creation and disposal of windows once the animation is done.

--- mlab_tools/animation.py
+++ mlab_tools/animation.py
@@ -1,3 +1,8 @@
+import glob
+import os
+import random
+import shutil
+
 from mayavi import mlab
 from tvtk.api import tvtk
 from tvtk.tools import visual
@@ -27,6 +32,8 @@ class StopAndRemove(AnimatorException):
 class Animation(object):
 
     def __init__(self, width, height):
+        self.width = width
+        self.height = height
         figure = mlab.figure(size=(width, height))
         visual.set_viewer(figure)
         self.obj_animations = dict()
@@ -52,15 +59,45 @@ class Animation(object):
 
         self.obj_animations[obj] = anim
 
-    def run(self, frame_callback=None, delay=30):
+    def _assemble_video(self, directory, filename, tmp_dir, framerate):
+        import cv2
 
-        @mlab.show
-        @mlab.animate(delay=delay)
+        video = cv2.VideoWriter('%s/%s.avi' % (directory, filename),
+                                cv2.cv.CV_FOURCC(*'XVID'),
+                                framerate,
+                                (self.width, self.height - 50))
+
+        frames = glob.glob('%s/*.png' % tmp_dir)
+
+        for i in xrange(1, len(frames)+1):
+            frame_img = cv2.imread('%s/%s_%d.png' % (tmp_dir, filename, i))
+            video.write(frame_img)
+
+        video.release()
+
+    def _save_frame(self, tmp_dir, filename, frame_no):
+        mlab.savefig('%s/%s_%d.png' % (tmp_dir, filename, frame_no))
+
+    def run(self, frame_callback=None, delay=30, save_to=None, framerate=30):
+
+        if save_to is not None:
+            try:
+                import cv2
+            except Exception:
+                print 'Python bindings for OpenCV needed to save animations!'
+                save_to = None
+            else:
+                directory, filename = os.path.split(save_to)
+                if not directory:
+                    directory = '.'
+                tmp_dir = '%s/tmp_%d' % (directory, random.randint(1,10**6))
+                os.mkdir(tmp_dir)
+
+        @mlab.animate(delay=delay, ui=False)
         def _run():
             frame_no = 1
 
             while True:
-
                 should_stop = True
 
                 for obj, anim in self.obj_animations.items():
@@ -82,13 +119,22 @@ class Animation(object):
                     except StopAnimation:
                         should_stop = True
 
-                if should_stop:
-                    break
+                if save_to is not None:
+                    self._save_frame(tmp_dir, filename, frame_no)
 
-                frame_no += 1
+                if should_stop:
+                    mlab.close(all=True)
+                    break
 
                 yield
 
-        _run()
-        
+                frame_no += 1
 
+        _ = _run()
+        mlab.show()
+
+        if save_to is not None:
+            self._assemble_video(directory, filename,
+                                 tmp_dir,
+                                 framerate)
+            shutil.rmtree(tmp_dir)
